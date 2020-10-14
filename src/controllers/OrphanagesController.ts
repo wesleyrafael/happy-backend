74599d129d304ca3d22853c2c52471dfe868422a
Wesley Rafael <wesley@MacBook-Air-de-Wesley.local>
2020-10-13 21:20:15 -0300

    adding support to image upload

--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -29,6 +29,11 @@ export default {
         } = request.body;
     
         const orphanagesRepository = getRepository(Orphanage);
+
+        const requestImages = request.files as Express.Multer.File[];
+        const images = requestImages.map(image => { 
+            return { path: image.filename };
+        });
     
         const orphanage = await orphanagesRepository.create({
             name, 
@@ -37,7 +42,8 @@ export default {
             about, 
             instructions, 
             opening_hours, 
-            open_on_weekends 
+            open_on_weekends,
+            images
         });
     
         await orphanagesRepository.save(orphanage);
--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
-import './database/connection';
 import OrphanagesController from './controllers/OrphanagesController';
+import multer from 'multer';
+import uploadConfig from './config/upload';
+
+const upload = multer(uploadConfig);
 
 const routes = Router();
 
-routes.post('/orphanages', OrphanagesController.create);
-routes.get('/orphanages/:id', OrphanagesController.show);
 routes.get('/orphanages', OrphanagesController.index);
+routes.get('/orphanages/:id', OrphanagesController.show);
+routes.post('/orphanages', upload.array('images'), OrphanagesController.create);
 
 export default routes;
